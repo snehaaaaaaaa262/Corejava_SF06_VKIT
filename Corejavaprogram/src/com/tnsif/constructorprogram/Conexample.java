0fc4ed528ca807ef06ad23d0d5d00ac6f3b33236
sneha <sneha@Sneha_lap1>
2025-10-28 12:01:28 +0530

    first

---
+++ Corejavaprogram/src/com/tnsif/constructorprogram/Conexample.java
@@ -0,0 +1,25 @@
+package com.tnsif.constructorprogram;
+
+public class Conexample {
+    Conexample(int i) {
+        System.out.println("constructor with 1 parameter");
+    }
+
+    Conexample(int i, int j) {
+        System.out.println("constructor with 2 parameter");
+    }
+
+    Conexample(int i, String g, int w) {
+        System.out.println("constructor with 3 parameter (int)");
+    }
+
+    Conexample(int i, String g, float w) {
+        System.out.println("constructor with 3 parameter (float)");
+    }
+
+    public static void main(String[] args) {
+        Conexample c1 = new Conexample(7, 9);
+        Conexample c2 = new Conexample(5);
+        Conexample c3 = new Conexample(7, "rash", 9.7f);
+    }
+}
---
+++ Corejavaprogram/src/com/tnsif/superkeyworddemo/Superdemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.superkeyworddemo;
+
+public class Superdemo {
+	
+	int a=60;// variable
+	
+	void drinking() {//method
+		System.out.println("tea");
+	}
+	 Superdemo(){ // constructor 
+		 System.out.println("welcome to java");
+	 }
+
+}
